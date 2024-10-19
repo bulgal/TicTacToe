f9a899100d8b545102d9aa2ab40eb3185d312069
Bulat Gal <b.a.galimuhametov.dev@gmail.com>
2024-10-19 19:11:28 +0200

    first commit

---
+++ Assets/Scripts/Box.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+    public int index;
+    public Mark mark;
+    public bool isMarked;
+
+    private SpriteRenderer SpriteRenderer;
+
+    private void Awake() {
+        SpriteRenderer = GetComponent<SpriteRenderer>();
+
+        index = transform.GetSiblingIndex();
+        isMarked = false;
+    }
+
+    public void SetAsMarked(Sprite sprite, Mark mark, Color color) {
+        isMarked = true;
+        this.mark = mark;
+
+        SpriteRenderer.color = color;
+        SpriteRenderer.sprite = sprite;
+
+        //Disable the CircleCollider2D (to avoid marking it twice)
+        GetComponent<CircleCollider2D>().enabled = false;
+    }
+}
